dd97e29d7f429de01b697409ef78069888ff60fe
Darby Lim <thlim@robotis.com>
2019-11-27 13:45:45 +0900

    Added tmux and tmux config

--- set_vim.sh
+++ set_vim.sh
@@ -21,6 +21,15 @@ sudo apt install vim -y
 cecho "Install Dependencies"
 sudo apt install build-essential cmake python3-dev clang-6.0 libclang-6.0-dev clang-format-6.0 python-clang-6.0 clang-tidy-6.0 libc++-dev lldb-6.0 lld-6.0 exuberant-ctags
 
+cecho "Install tmux"
+sudo apt install tmux -y
+
+cecho "Intall tmux config"
+cd
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local .
+
 cecho "Install Vundle"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
