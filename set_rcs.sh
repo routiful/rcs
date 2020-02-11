5f2447a13dcd2a333e0683e80855a14e483f3819
Darby Lim <routiful@gmail.com>
2020-02-11 16:32:28 +0900

    Added enters

--- set_rcs.sh
+++ set_rcs.sh
@@ -20,7 +20,19 @@ cecho "Install VIM"
 sudo apt install vim -y
 
 cecho "Install Some Dependencies"
-sudo apt install build-essential cmake python3-dev clang-6.0 libclang-6.0-dev clang-format-6.0 python-clang-6.0 clang-tidy-6.0 libc++-dev lldb-6.0 lld-6.0 exuberant-ctags
+sudo apt install -y \
+  build-essential \
+  cmake \
+  python3-dev \
+  clang-6.0 \
+  libclang-6.0-dev \
+  clang-format-6.0 \
+  python-clang-6.0 \
+  clang-tidy-6.0 \
+  libc++-dev \
+  lldb-6.0 \
+  lld-6.0 \
+  exuberant-ctags
 
 cecho "Install Some tools"
 sudo apt install -y \
